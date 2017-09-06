16bfe656b00d8f03f84412a3ec93d684db51c1a3
cnsheafe <cnsheafe@gmail.com>
2017-09-06 13:47:27 -0500

    init

---
+++ src/components/BlogList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="bloglist">
+    <form action="">
+      Title<input type="text" v-model="title" /> Author
+      <input type="text" v-model="author" /> Body
+      <input type="text" v-model="body" />
+      <button @submit="AddPost()">Add</button>
+    </form>
+    <ul id="posts">
+      {{this.posts}}
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+  methods: {
+    AddPost() {
+      this.posts.push({
+        title: this.title,
+        author: this.author,
+        body: this.body
+      });
+    }
+  },
+  mounted() { }
+  ,
+  components: {
+    "BlogPost": require("./BlogPost")
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2 class="blogpost-title">{{title}}</h2>
+        <h3 class="blogpost-author">{{author}}</h3>
+        <p class="blogpost-body">{{body}}</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            title: "Blogpost Title",
+            author: "John Doe",
+            body: "Hello!"
+        }
+    },
+    mounted() {
+      
+  }
+}
+</script>
